49a8e10ac0afa5dff30cfbefc9d96e197337f4b0
afaqkhan707 <bsse5482@gmail.com>
2023-12-18 19:25:47 +0500

    todowithRTK

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <>
+      <div className='flex h-screen w-screen bg-slate overflow-hidden items-center justify-center'>
+        <div className='todo-box'>
+          <div className='add-todo'>
+            <h1>ReduxToolKit</h1>
+            <AddTodo />
+          </div>
+          <div className='todo-list'>
+            <Todos />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
